5669c60f33455f60ab04217b38e8840e9bb72e96
zhujunxi <531578783@qq.com>
2019-11-21 15:37:34 +0800

    chore: format code

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(ViewUI);
+Vue.use(ViewUI)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
